5d0fa698872f5edd2549d79d2424029b83ff4b43
Rabka <smot@itu.dk>
2014-05-09 20:53:42 +0200

    Building start

---
+++ Assets/Stinus/Building.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Building : MonoBehaviour {
+
+	public Transform BuildingBlock;
+	private int blockSide = 16;
+
+	public int white = 4;
+	public int hight = 8;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
