dfac0eb495ed0f2ffb71238448d4bd792502cf78
Rabka <smot@itu.dk>
2014-05-10 15:59:31 +0200

    her

--- PyGame/eksplosion.py
+++ PyGame/eksplosion.py
@@ -1,4 +1,24 @@
 
-from simplegame import Game, Transform, Axie
-import 
+from simplegame import Game, Transform, Axis
+import pygame
 import os
+
+class Explosion(Transform):
+	def __init__(self, pos):
+		super().__init__(pygame.Rect(0,0,0,0), pos=pos)
+		self.image = Animation("bAnim").play(True)
+	
+	def render(self, surf):
+		surf.blit(self.image, self.drawpos)
+
+
+def main():
+	size = (800, 600)
+	game = Game(size, "Kaijuu Game")
+	
+	
+	
+	
+	
+if __name__=="__main__":
+	main()
