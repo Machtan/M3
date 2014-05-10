0e854f60b266b5ee7c4120229c2c5b628fedf619
Rabka <smot@itu.dk>
2014-05-10 21:24:09 +0200

    her

--- PyGame/projectile.py
+++ PyGame/projectile.py
@@ -7,6 +7,13 @@ import math
 degconv = 180/math.pi
 gravity = Vector(0,0.1)
 
+def angelBetweenVektors(other)
+	degconv = 180/math.pi
+	prikProdukt = (self.vec.x * other.x) + (self.vec.y * other.y)
+	thisLength = math.sqrt(self.vec.x**2 + self.vec.y**2)
+	otherLength = math.sqrt(other.x**2 + other.y**2)
+	return math.acos(prikProdukt/(thisLength * otherLength)) * degconv
+
 class Missile(Rotatable):
 	def __init__(self, pos, vec):
 		super().__init__(pos, "missile", 0)
